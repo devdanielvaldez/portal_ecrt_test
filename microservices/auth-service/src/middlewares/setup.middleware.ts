5084e3d82feb8c44b09bc50381d31fb636592eb2
Daniel Valdez | FullStack Developer <hello@daniel-valdez.com>
2026-05-06 16:31:40 -0400

    fix

--- microservices/auth-service/src/middlewares/setup.middleware.ts
+++ microservices/auth-service/src/middlewares/setup.middleware.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const requireMasterKey = (req: Request, res: Response, next: NextFunction): void => {
+  console.log(process.env);
+  
   const masterKey = req.headers['x-master-key'];
   if (!masterKey || masterKey !== process.env.SETUP_MASTER_KEY || 'Prueba01*') {
     res.status(403).json({ error: 'Invalid or missing master key' });
